a7729c094736b009f43c956634e553314191c18d
Pranjul Jani <49308187+pranjul-jani@users.noreply.github.com>
2021-10-26 19:38:43 +0530

    deleted non related classes

--- src/FakeConnectionDriver.java
+++ src/FakeConnectionDriver.java
@@ -2,7 +2,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class FakeConnectionApp {
+public class FakeConnectionDriver {
 
     public static void main(String[] args) throws InterruptedException {
 
